bc4594aeee16342e2219e1d52c2ffbdb780514b1
felipe <felipetiagodecarli@hotmail.com>
2020-05-08 21:45:21 -0300

    add docs tests

--- jogo-da-vida/src/test/java/BoardTestCase.java
+++ jogo-da-vida/src/test/java/BoardTestCase.java
@@ -7,64 +7,63 @@ import org.junit.Test;
 
 public class BoardTestCase {
 	
+	private int[][][] boardSetup0 = new int[][][] {
+		{ //input
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 1, 1, 0, 0, 0 },
+			{ 0, 0, 0, 0, 1, 1 },
+			{ 1, 0, 0, 0, 1, 0 },
+			{ 1, 1, 0, 0, 0, 0 },
+			{ 1, 1, 0, 0, 0, 1 }
+		},
+		{ //expected
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 1, 0, 1, 1, 1 },
+			{ 1, 1, 0, 0, 1, 1 }, 
+			{ 0, 0, 0, 0, 0, 0 }, 
+			{ 1, 1, 0, 0, 0, 0 }
+		}
+	};
+	
+	private int[][][] boardSetup1 = new int[][][] {
+		{
+			{ 0, 0, 0, 1, 0, 1 },
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 1, 0, 1 },
+			{ 1, 0, 0, 0, 0, 0 },
+			{ 1, 1, 0, 0, 0, 0 },
+			{ 1, 1, 0, 0, 0, 0 }
+		},
+		{
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 1, 1, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 1, 1, 0, 0, 0, 0 }
+		}
+	};
 
-	private int[][] finalBoard = new int[][]{
-		  { 0, 0, 0, 0, 0, 0 },
-		  { 0, 0, 0, 0, 0, 0 },
-		  { 0, 0, 0, 0, 0, 0 },
-		  { 0, 0, 0, 0, 0, 0 },
-		  { 0, 0, 0, 0, 0, 0 },
-		  { 0, 0, 0, 0, 0, 0 }
-	};
-	
-	private int[][] boardSetup1 = new int[][] {
-		{ 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 0, 0, 1 },
-		{ 0, 1, 1, 0, 1, 1 },
-		{ 0, 1, 1, 1, 0, 1 },
-		{ 1, 0, 0, 1, 0, 1 },
-		{ 0, 0, 1, 1, 0, 1 }
-	};
-	
-	private int[][] boardSetup1Next = new int[][] {
-		{ 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 0, 1, 1 },
-		{ 0, 1, 0, 0, 0, 1 },
-		{ 1, 0, 0, 0, 0, 1 },
-		{ 0, 0, 0, 0, 0, 1 },
-		{ 0, 0, 1, 1, 0, 0 }
-	};
 	
 	@Test
-	public void boardLengthTest() {
+	public void boardSetup0Test() {
 		Board board = new Board();
-		int expectedSize = 6;
-		int obtained = board.getBoard().length;
-		assertEquals(expectedSize, obtained);
-	}
-	
-	@Test
-	public void finalBoardStateTest() {
-		Board board = new Board();
-		int[][] expectedArray = finalBoard;
-		board.setBoard(expectedArray);
-		// board.nextState() should not alter the expectedArray
+		board.setBoard(boardSetup0[0]);
 		board.nextState();
 		// Since there is no method for testing 2d arrays, we need to assert each row
 		for (int i = 0; i < 6; i++) {
-			assertArrayEquals(expectedArray[i], board.getBoard()[i]);
+			assertArrayEquals(boardSetup0[1][i], board.getBoard()[i]);
 		}
 	}
-	
 	@Test
-	public void generalBoardStateTest() {
+	public void boardSetup1Test() {
 		Board board = new Board();
-		int[][] expectedArray = boardSetup1Next;
-		board.setBoard(boardSetup1);
+		board.setBoard(boardSetup1[0]);
 		board.nextState();
 		// Since there is no method for testing 2d arrays, we need to assert each row
 		for (int i = 0; i < 6; i++) {
-			assertArrayEquals(expectedArray[i], board.getBoard()[i]);
+			assertArrayEquals(boardSetup1[1][i], board.getBoard()[i]);
 		}
 	}
 
